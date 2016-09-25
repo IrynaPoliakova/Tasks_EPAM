34700d6484659e73f193090c39ca28db2ea321e8
Iryna Poliakova <irinapolyakova1994@gmail.com>
2016-09-26 00:23:54 +0300

    task 8 - Denis - reflection

--- reflection19/src/main/java/ua/epam/MyFirstAnn.java
+++ reflection19/src/main/java/ua/epam/MyFirstAnn.java
@@ -8,5 +8,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface MyFirstAnn {
-    String title() default "Smile:)";
+    String title() default "Smile:))";
 }
