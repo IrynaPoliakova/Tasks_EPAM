5973665f083c2f022087d87c4a68daf82af9a9f0
Iryna Poliakova <Iryna_Poliakova@epam.com>
2016-09-26 13:10:28 +0300

    task 10

--- task10/src/test/java/ua/epam/Task10Test.java
+++ task10/src/test/java/ua/epam/Task10Test.java
@@ -1,6 +1,8 @@
 package ua.epam;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import ua.epam.Entity.*;
 
 import java.util.Date;
@@ -10,12 +12,15 @@ import static org.junit.Assert.*;
 /**
  * Created by irinapoliakova on 26.09.16.
  */
-public class MainTest {
+public class Task10Test {
+    @Rule
+    public final ExpectedException exp = ExpectedException.none();
+
     @Test
     public void creatRouteWithCorrectData() throws Exception {
         Human john = new Human("John" , new Date() , Human.Gender.MALE , Human.BloodGroup.FIRST);
-        Driver johnDriver = new Driver(john ,new DriverLicense(new Date() ,
-                DriverLicense.Category.C , new Date()));
+        Driver johnDriver = new Driver(john ,new DriverLicense(new Date(40,03,10) ,
+                DriverLicense.Category.C , new Date(70,03,10)));
         GasolineCar car = new GasolineCar("ZAZ", "AA5555AA", "100500" , DriverLicense.Category.C ,
                 1499 , 80 , GasolineCar.Transmission.AUTOMATIC);
         Route route = new Route(car , johnDriver , "Troeshina" , "Klovska");
@@ -25,8 +30,8 @@ public class MainTest {
     @Test
     public void creatRouteWithLicenseIncompatibility() throws Exception {
         Human john = new Human("John" , new Date() , Human.Gender.MALE , Human.BloodGroup.FIRST);
-        Driver johnDriver = new Driver(john ,new DriverLicense(new Date() ,
-                DriverLicense.Category.B , new Date()));
+        Driver johnDriver = new Driver(john ,new DriverLicense(new Date(40,03,10) ,
+                DriverLicense.Category.B , new Date(70,03,10)));
         GasolineCar car = new GasolineCar("ZAZ", "AA5555AA", "100500" , DriverLicense.Category.C ,
                 1499 , 80 , GasolineCar.Transmission.AUTOMATIC);
         Route route = new Route(car , johnDriver , "Troeshina" , "Klovska");
@@ -34,14 +39,26 @@ public class MainTest {
     }
 
     @Test
-    public void creatRouteNullHuman() throws Exception {
-        Human john = null;
-        Driver johnDriver = new Driver(john ,new DriverLicense(new Date() ,
-                DriverLicense.Category.B , new Date()));
+    public void creatLisenceNullHuman() throws Exception {
+        exp.expect(IllegalArgumentException.class);
+        Human john = new Human("John" , new Date() , Human.Gender.MALE , Human.BloodGroup.FIRST);
+        Driver johnDriver = null;
         GasolineCar car = new GasolineCar("ZAZ", "AA5555AA", "100500" , DriverLicense.Category.C ,
                 1499 , 80 , GasolineCar.Transmission.AUTOMATIC);
         Route route = new Route(car , johnDriver , "Troeshina" , "Klovska");
         System.out.println(route);
     }
+
+    @Test
+    public void checkDateExpiresEarlierFrom() throws Exception {
+        Human john = new Human("John" , new Date() , Human.Gender.MALE , Human.BloodGroup.FIRST);
+        Driver johnDriver = new Driver(john ,new DriverLicense(new Date(70,03,10) ,
+                DriverLicense.Category.B , new Date(40,03,10)));
+        GasolineCar car = new GasolineCar("ZAZ", "AA5555AA", "100500" , DriverLicense.Category.C ,
+                1499 , 80 , GasolineCar.Transmission.AUTOMATIC);
+        Route route = new Route(car , johnDriver , "Troeshina" , "Klovska");
+        System.out.println(route);
+    }
+
 
 }
